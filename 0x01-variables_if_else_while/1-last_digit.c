ccbc7efe2caadf040e214cff618b0c69c590e92f
Aminat Animashaun <aminatanimashaun31@aminatanimashaun31@gmail.com>
2022-09-12 18:22:24 -0700

    variables, if, else

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -8,13 +8,13 @@
 
 /**
  *
- *  * main - entry point
+ *  * main - Entry point
  *
  *   *
  *
  *    *
  *
- *     * Return: always return 0
+ *     * Return: always Return 0
  *
  *      *
  *
@@ -24,76 +24,40 @@ int main(void)
 
 {
 
-	        int n;
+		int n;
 
-		        int lastn;
-
-
-
-			        srand(time(0));
-
-				        n = rand() - RAND_MAX / 2;
-
-					        lastn = n % 10;
+			int lastn;
 
 
 
-						        if (lastn > 5)
+				srand(time(0));
 
-								        {
+					n = rand() - RAND_MAX / 2;
 
-										                printf("Last digit of %d is %d and is greater than 5\n", n, lastn);
+						lastn = n % 10;
 
-												        } else if (lastn == 0)
 
-														        {
 
-																                printf("Last digit of %d is %d and is 0\n", n, lastn);
+							if (lastn > 5)
 
-																		        } else
+									{
 
-																				        {
+												printf("Last digit of %d is %d and is greater than 5\n", n, lastn);
 
-																						                printf("Last digit of %d is %d and is less than 6 and not 0\n", n, lastn);
+													} else if (lastn == 0)
 
-																								        }
+															{
 
-																			        return (0);
+																		printf("Last digit of %d is %d and is 0\n", n, lastn);
+
+																			} else
+
+																					{
+
+																								printf("Last digit of %d is %d and is less than 6 and not 0\n", n, lastn);
+
+																									}
+
+																				return (0);
 
 }
-
-[24:46, 9/10/2022] +234 902 377 2493: Task 2
-
-
-
-#include <stdio.h>
-
-/**
- *
- *  * main - entry point
- *
- *   *
- *
- *    * Return: alway returns 0
- *
- *     *
- *
- *      */
-
-int main(void)
-
-{
-
-	        char a;
-
-
-
-		        for (a = 'a'; a <= 'z' ; a++)
-
-				                putchar(a);
-
-			        putchar('\n');
-
-				        return (0);
-
-
